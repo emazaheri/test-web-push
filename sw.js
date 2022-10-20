9e4d2af7163c7610de87760c526b933b937f7283
Ehsan <ehsanmzht@gmail.com>
2022-10-20 19:58:24 +0330

    update service worker

--- sw.js
+++ sw.js
@@ -1,8 +1,13 @@
-self.addEventListener('push', () => {
-    console.log('push event');
-    self.registration.showNotification('test title', {
-        body: 'test body',
-        icon: 'https://cheegol.ir/favicon.ico'
-    });
-}
-);
+self.addEventListener('push', function (event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+    const title = 'Push Codelab';
+    const options = {
+        body: 'Yay it works.',
+        icon: 'images/icon.png',
+        badge: 'images/badge.png'
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
+});
