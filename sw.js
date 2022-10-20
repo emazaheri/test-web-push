579b11169dc3f3e09378ed1e7e6e7a0b622f635b
Ehsan <ehsanmzht@gmail.com>
2022-10-20 23:54:33 +0330

    update service worker

--- sw.js
+++ sw.js
@@ -1,13 +1,38 @@
-self.addEventListener('push', function (event) {
-    console.log('[Service Worker] Push Received.');
-    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+self.addEventListener('push', function(event) {
+	logger.log('Push message received.');
+	let notificationTitle = 'Hello';
+	const notificationOptions = {
+		body: 'Thanks for sending this push msg.',
+		icon: 'https://app.cheegol.ir/favicon.ico',
+		// badge: './images/badge-72x72.png',
+		tag: 'simple-push-demo-notification',
+		data: {
+			url: 'https://web.dev/push-notifications-overview/',
+		},
+	};
 
-    const title = 'Push Codelab';
-    const options = {
-        body: 'Yay it works.',
-        icon: 'images/icon.png',
-        badge: 'images/badge.png'
-    };
+	if (event.data) {
+		const dataText = event.data.text();
+		notificationTitle = 'Received Payload';
+		notificationOptions.body = `Push data: '${dataText}'`;
+	}
 
-    event.waitUntil(self.registration.showNotification(title, options));
+	event.waitUntil(
+		self.registration.showNotification(
+			notificationTitle,
+			notificationOptions,
+		),
+	);
+});
+
+self.addEventListener('notificationclick', function(event) {
+	logger.log('Notification clicked.');
+	event.notification.close();
+
+	let clickResponsePromise = Promise.resolve();
+	if (event.notification.data && event.notification.data.url) {
+		clickResponsePromise = clients.openWindow(event.notification.data.url);
+	}
+
+	event.waitUntil(clickResponsePromise);
 });
